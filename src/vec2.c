4ca89c3fb5132e776380dcb21990d0f52993e976
Willehard Korander <willehard@gmail.com>
2019-12-06 14:49:09 +0200

    ...

--- src/vec2.c
+++ src/vec2.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 14:21:07 by wkorande          #+#    #+#             */
-/*   Updated: 2019/12/06 14:22:35 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/12/06 14:45:28 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ t_vec2	make_vec2(float x, float y)
 t_intvec2 make_intvec2(int x, int y)
 {
 	t_intvec2 v;
+
 	v.x = x;
 	v.y = y;
 	return (v);
--- src/vec3.c
+++ src/vec3.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 14:18:40 by wkorande          #+#    #+#             */
-/*   Updated: 2019/12/06 14:30:01 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/12/06 14:48:50 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,8 @@ t_vec3	make_vec3_rot(float x, float y, float z)
 	return (v);
 }
 
-/*
-**	Transforms point to world space. Applies rotation and scale.
-*/
-
 t_vec3		transform_point(t_vec3 v, t_vec3 translate, t_vec3 rot, t_vec3 scale)
 {
-	// glm::mat4 myModelMatrix = myTranslationMatrix * myRotationMatrix * myScaleMatrix;
-	// glm::vec4 myTransformedVector = myModelMatrix * myOriginalVector;
-	//v = multiply_matrix_vec3(v, create_rotation_matrix_xyz(rot));
 	v = multiply_matrix_vec3(v, create_translation_matrix(translate));
 	v = multiply_matrix_vec3(v, create_scaling_matrix(scale));
 	return (v);
@@ -55,5 +48,13 @@ t_vec3		translate_point_3d(t_vec3 p, t_vec3 translation)
 	v.x += translation.x;
 	v.y += translation.y;
 	v.z += translation.z;
+	return (v);
+}
+
+t_vec3	multiply_vec3(t_vec3 v, float f)
+{
+	v.x *= f;
+	v.y *= f;
+	v.z *= f;
 	return (v);
 }
